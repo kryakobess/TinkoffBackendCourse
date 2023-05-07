da28bad1950b3177f4b63401987885b7e2bcc5d2
kryakobess <martyz89@yandex.ru>
2023-05-07 16:35:47 +0300

    bot List and Unknown command test fixes + bot dockerfile

--- bot/src/test/java/bot/services/TelegramBot/commands/ListCommandMock.java
+++ bot/src/test/java/bot/services/TelegramBot/commands/ListCommandMock.java
@@ -1,19 +1,34 @@
 package bot.services.TelegramBot.commands;
 
+import bot.DTOs.responses.LinkScrapperResponse;
+import bot.services.ScrapperClient;
+import org.mockito.Mockito;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.stereotype.Component;
 
+import java.net.URI;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
 @Component
 public class ListCommandMock {
     @SpyBean
     public ListCommand listCommand;
+
+    @SpyBean
+    ScrapperClient client;
 
     public ListCommand getListCommand() {
         return listCommand;
     }
 
     public void setLinksList(List<String> links){
-        listCommand.links = links;
+        when(client.getLinks(any()).links())
+                .thenReturn(links.stream()
+                        .map(l -> new LinkScrapperResponse(1, URI.create(l)))
+                        .collect(Collectors.toList()));
     }
 }
--- bot/src/test/java/bot/services/TelegramBot/commands/ListCommandTest.java
+++ bot/src/test/java/bot/services/TelegramBot/commands/ListCommandTest.java
@@ -1,5 +1,8 @@
 package bot.services.TelegramBot.commands;
 
+import bot.DTOs.responses.LinkScrapperResponse;
+import bot.DTOs.responses.ListLinksScrapperResponse;
+import bot.services.ScrapperClient;
 import com.pengrad.telegrambot.model.Chat;
 import com.pengrad.telegrambot.model.Message;
 import com.pengrad.telegrambot.model.Update;
@@ -12,23 +15,31 @@ import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
+@SpringBootTest
 class ListCommandTest {
 
     @Mock
     Update update;
 
-    @Spy
+    @Autowired
     ListCommand listCommand;
+
+    @MockBean
+    ScrapperClient scrapperClient;
 
     @BeforeEach
     public void setUp(){
@@ -44,8 +55,9 @@ class ListCommandTest {
     @Test
     void handle_withNoLinks() {
         //given
-        List<String> links = new ArrayList<>();
-        listCommand.links = links;
+        when(scrapperClient.getLinks(anyLong())).thenReturn(new ListLinksScrapperResponse(0, new ArrayList<>()));
+
+       // when(listCommand.scrapperClient).thenReturn(scrapperClient);
 
         //when
         SendMessage sendMessage = listCommand.handle(update);
@@ -60,8 +72,11 @@ class ListCommandTest {
                         "https://github.com/kryakobess/TinkoffBackendCourse/pulls",
                         "https://www.youtube.com/watch?v=SjDMW-6vh58"
                 );
-        listCommand.links = links;
-        String expectedMessage = String.join("\n\n", links);
+        when(listCommand.scrapperClient.getLinks(anyLong())).thenReturn(
+                new ListLinksScrapperResponse(anyInt(), links.stream()
+                .map(l -> new LinkScrapperResponse(1, URI.create(l)))
+                .collect(Collectors.toList())));
+        String expectedMessage = String.join("\n", links);
 
         //when
         SendMessage sendMessage = listCommand.handle(update);
